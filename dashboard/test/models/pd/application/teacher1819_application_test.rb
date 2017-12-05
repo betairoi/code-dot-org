30086c74411e5d551e76d57b0998a17b604b2671
Elijah Hamovitz <elijahhamovitz@gmail.com>
2017-12-04 17:54:10 -0800

    add application decision notification mailer tests

--- dashboard/test/models/pd/application/teacher1819_application_test.rb
+++ dashboard/test/models/pd/application/teacher1819_application_test.rb
@@ -30,5 +30,47 @@ module Pd::Application
       assert_equal 'Dr. Dumbledore', application_with_principal_title.principal_greeting
       assert_equal 'Albus Dumbledore', application_without_principal_title.principal_greeting
     end
+
+    test 'send_decision_notification_email only sends to G3 and unmatched' do
+      application = create :pd_teacher1819_application
+      application.update(status: 'accepted')
+
+      mock_mail = stub
+      mock_mail.stubs(:deliver_now).returns(nil)
+
+      Pd::Application::Teacher1819ApplicationMailer.expects(:accepted).times(2).returns(mock_mail)
+      application.send_decision_notification_email
+
+      partner = create :regional_partner
+      application.update(regional_partner: partner)
+
+      partner.update(group: 1)
+      application.send_decision_notification_email
+
+      partner.update(group: 2)
+      application.send_decision_notification_email
+
+      partner.update(group: 3)
+      application.send_decision_notification_email
+    end
+
+    test 'send_decision_notification_email only sends to finalized' do
+      mock_mail = stub
+      mock_mail.stubs(:deliver_now).returns(nil)
+
+      Pd::Application::Teacher1819ApplicationMailer.expects(:pending).times(0)
+      Pd::Application::Teacher1819ApplicationMailer.expects(:unreviewed).times(0)
+      Pd::Application::Teacher1819ApplicationMailer.expects(:withdrawn).times(0)
+
+      Pd::Application::Teacher1819ApplicationMailer.expects(:accepted).times(1).returns(mock_mail)
+      Pd::Application::Teacher1819ApplicationMailer.expects(:declined).times(1).returns(mock_mail)
+      Pd::Application::Teacher1819ApplicationMailer.expects(:waitlisted).times(1).returns(mock_mail)
+
+      application = create :pd_teacher1819_application
+      Pd::Application::Teacher1819Application.statuses.values.each do |status|
+        application.update(status: status)
+        application.send_decision_notification_email
+      end
+    end
   end
 end
